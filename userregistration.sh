c819f0bab5685a156cc31a4167e7321600665af6
vaishnavi586 <machavolu1996@gmail.com>
2020-11-09 09:34:12 +0530

    added validating status of email

--- userregistration.sh
+++ userregistration.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -x
 pat="^[0-9]*[a-zA-Z]{3}[1-9a-zA-Z]*$"
+pat1="^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}+([.][a-zA-Z]{2,3})?$"
 echo "enter username"
 read anyusername
-if [[ $any =~ $pat ]];
+echo "enter emailid"
+read  emailid
+if [[ $any =~ $pat && $emailid =~ $pat1 ]];
 then
      echo vaild
 else
