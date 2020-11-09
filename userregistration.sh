4a3844015d3c828b9569e61307665294f2fdee19
vaishnavi586 <machavolu1996@gmail.com>
2020-11-09 09:41:10 +0530

    added the mobile no validation

--- userregistration.sh
+++ userregistration.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -x
 pat="^[0-9]*[a-zA-Z]{3}[1-9a-zA-Z]*$"
 pat1="^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[0-9a-zA-Z]+.[a-zA-Z]{2,4}+([.][a-zA-Z]{2,3})?$"
+pat2="^[0-9]{2} [0-9]{10}"
 echo "enter username"
 read anyusername
 echo "enter emailid"
 read  emailid
-if [[ $any =~ $pat && $emailid =~ $pat1 ]];
+echo "enter mobileno"
+read mobileno
+if [[ $any =~ $pat && $emailid =~ $pat1 && $mobileno =~ $pat2 ]];
 then
      echo vaild
 else
